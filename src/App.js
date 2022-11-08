52b34322d86889a676a40db0ce45d01c731843fa
Nan Chen <nan_chen@brown.edu>
2022-11-08 05:08:57 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,65 @@
 import "./App.css";
-import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from "./components/BakeryItem";
+import {useState} from 'react';
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
-  item.image = process.env.PUBLIC_URL + "/" + item.image;
+    item.image = process.env.PUBLIC_URL + "/" + item.image;
 });
+
 /* ############################################################## */
 
 function App() {
-  // TODO: use useState to create a state variable to hold the state of the cart
-  /* add your cart state code here */
+    // TODO: use useState to create a state variable to hold the state of the cart
+    // initial cart value as zero
+    const [cart, setCart] = useState([]);
 
-  return (
-    <div className="App">
-      <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
+    function handleClick(item) {
+        setCart([...cart, item]);
+    }
 
-      {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
-      ))}
-
-      <div>
-        <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
-      </div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="row">
+                <div className="col-sm-7">
+                    <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
+                    {/* set each line has three columns */}
+                    <div className="row row-cols-1 row-cols-md-2 g-4">
+                        {bakeryData.map((item, index) => (
+                            <BakeryItem
+                                key={index}
+                                name={item.name}
+                                description={item.description}
+                                price={item.price}
+                                image={item.image}
+                                handleClick={handleClick}
+                            />
+                        ))}
+                    </div>
+                </div>
+                <div className="col-sm-5">
+                    <div>
+                        <h2>Cart</h2>
+                        {/* TODO: render a list of items in the cart */}
+                        {/* if the cart is empty, display a text message: Nothing here just yet! */}
+                        {/* if the cart is not empty, display a list of items in the cart */}
+                        {(cart.length === 0) ? <p>Nothing here just yet!</p> : <div>
+                            <ul className="list-group col-sm-10">
+                                {cart.map((item, index) => (
+                                    <li className="list-group-item" key={index}>
+                                        {item.name} - ${item.price}
+                                    </li>
+                                ))}
+                            </ul>
+                            {/* show the total price of the items in the cart, keep two decimal places */}
+                            <h3>Total: ${cart.reduce((total, item) => total + item.price, 0).toFixed(2)}</h3>
+                        </div>}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,22 @@
 // TODO: create a component that displays a single bakery item
+// use card from bootstrap to show the image, description, name, and price, and an add to cart (bootstrap button) button
+// data (image, description, name, price) is passed in as props
+
+// modify app.js cart when add to cart button is clicked
+
+
+export default function BakeryItem(props) {
+    return (
+        <div className="col-sm-6">
+            <div className="card">
+                <img src={props.image} className="card-img-top" alt={props.name}/>
+                <div className="card-body">
+                    <h5 className="card-title">{props.name}</h5>
+                    <p className="card-text">{props.description}</p>
+                    <p className="card-text">${props.price}</p>
+                    <button className="btn btn-primary" onClick={() => props.handleClick(props)}>Add to Cart</button>
+                </div>
+            </div>
+        </div>
+    );
+}
